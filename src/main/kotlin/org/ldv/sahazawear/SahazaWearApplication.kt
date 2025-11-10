2da60a3297088c6e28ba739fbea664bbcdce4468
sahaza77 <sahaza.pro@gmail.com>
2025-11-10 09:19:43 +0100

    first commit

---
+++ src/main/kotlin/org/ldv/sahazawear/SahazaWearApplication.kt
@@ -0,0 +1,11 @@
+package org.ldv.sahazawear
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SahazaWearApplication
+
+fun main(args: Array<String>) {
+    runApplication<SahazaWearApplication>(*args)
+}
---
+++ src/test/kotlin/org/ldv/sahazawear/SahazaWearApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ldv.sahazawear
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SahazaWearApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
